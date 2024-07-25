565d48d4aae66e2c1579a95f135cf7f9d3cdbc40
parmar5855 <pawanparmar5855@gmail.com>
2024-07-25 18:37:45 +0530

    first commit

---
+++ src/bankManagementSystem/Conn.java
@@ -0,0 +1,25 @@
+package bankManagementSystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+
+public class Conn {
+	
+	Connection connection;
+	Statement statement;
+	
+	public Conn(){
+		try {
+			
+			connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/banksystem","root","pawan123");
+			statement= connection.createStatement();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
